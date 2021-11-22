ce6f374c0f58322e41baf679a898eb1af498edb1
Shinya-works <shinya.works.w@gmail.com>
2021-11-22 10:13:57 +0000

    パスワードのハッシュ化と最小文字の設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211122094405) do
+ActiveRecord::Schema.define(version: 20211122100141) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
